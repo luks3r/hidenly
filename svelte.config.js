a0d3dd9e3e5c75c5556784af2b01c9e33206654b
Sergei Lukaushkin <sergeilukaushkin@gmail.com>
2023-06-04 02:45:25 +0300

    fix: add default base path

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,6 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
@@ -17,7 +15,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-            base: dev ? '' : process.env.BASE_PATH,
+            base: process.env.BASE_PATH ?? '/hidenly',
         }
 	}
 };
