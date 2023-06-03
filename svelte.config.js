e61953ee33e408559f059fbdf13ff4f1f4e6146b
Sergei Lukaushkin <sergeilukaushkin@gmail.com>
2023-06-04 02:56:54 +0300

    fix: add vercel support

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import preprocess from 'svelte-preprocess';
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,7 +17,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-            base: process.env.BASE_PATH ?? '/hidenly',
+            base: dev ? '' : process.env.BASE_PATH,
         }
 	}
 };
